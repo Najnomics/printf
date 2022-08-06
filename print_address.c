2712be4e0a576405fc099ca9b9f7aa30ed23102a
Praise Dike <dikepraise119@gmail.com>
2022-08-06 01:09:13 -0700

    print adress

---
+++ print_address.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int print_address(va_list l, flags_t *f)
+{
+	char *str;
+	unsigned long int p = va_arg(l, unsigned long int);
+
+	register int count = 0;
+
+	(void)f;
+
+	if (!p)
+		return (_puts("(nil)"));
+	str = convert(p, 16, 1);
+	count += _puts("0x");
+	count += _puts(str);
+	return (count);
+}
